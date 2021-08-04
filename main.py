63a4095c3ef2ad43d4701681926ab232a7b9474d
/wizu <88033568+wizu-but-coding@users.noreply.github.com>
2021-08-04 12:58:08 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+import requests
+from discord_webhooks import DiscordWebhooks
+
+webhook_url = input('Enter WEBHOOK URL: ')
+
+message = input('Enter Message: ')
+ilosc = int(input('Enter Amount of Messages: '))
+
+
+webhook = DiscordWebhooks(webhook_url)
+
+"content"
+  
+
+
+
+for i in range(ilosc):
+    webhook.send()
+
+
+#made by "!  ˖𓆩ƁƬ⌁ WIZΛRÐ ♡ †ᵍᵒᵈ#0009
+#https://discord.gg/btop
